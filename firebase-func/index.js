4ae61f65188b0ad8c4479a7fc14c44cbd40fbbb4
itinao <itinao@gmail.com>
2018-05-31 03:12:12 +0900

    parse

--- firebase-func/index.js
+++ firebase-func/index.js
@@ -1,24 +1,35 @@
 const functions = require('firebase-functions');
 const url = require('url');
 const request = require('request');
+const FeedParser = require('feedparser');
 
-const headers = {
-  'Content-Type':'application/xml'
-};
-
-const options = {
-  method: 'GET',
-  headers: headers,
-};
+const feedparser = new FeedParser({});
 
 exports.rss = functions.https.onRequest((req, res) => {
   const orgUrl = url.parse(req.originalUrl).query;
+  const items = [];
 
   if (req.method === "GET") {
-    options["url"] = orgUrl;
-    request(options, function (error, response, body) {
-      res.status(200).send(body);
-    })
+    const feedReq = request(orgUrl);
+    feedReq.on('response', function (response) {
+      this.pipe(feedparser)
+        .on('error', function(error) {
+          response.status(500).json({
+            'message': 'HTTP failure while fetching feed'
+          });
+        })
+        .on('meta', function(meta) {
+        })
+        .on('readable', function() {
+          while(item = this.read()) {
+            items.push({title: item.title, link: item.link});
+          }
+        })
+        .on('end', function() {
+          res.status(200).send(items);
+        });
+    });
+
   } else {
     res.status(404).end();
   }

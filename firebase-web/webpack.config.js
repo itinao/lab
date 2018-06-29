1741bbe97b695df8b198e5d86b3b27d464b9ed5d
itinao <itinao@gmail.com>
2018-06-29 19:31:07 +0900

    add firebase prj

--- firebase-web/webpack.config.js
+++ firebase-web/webpack.config.js
@@ -1,16 +1,28 @@
 const path = require('path');
 
+const project = {
+  base(...args) {
+    return path.join(__dirname, ...args);
+  },
+  src(...args) {
+    return this.base("src/ts/", ...args);
+  },
+  dist(...args) {
+    return this.base("public/assets/", ...args);
+  }
+};
+
 module.exports = {
   mode: 'development',
-  entry: './src/ts/app.ts',
+  entry: project.src('app.tsx'),
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public/assets/js')
+    path: project.dist('js')
   },
   module: {
     rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader',
+      test: /\.tsx$/,
+      use: 'awesome-typescript-loader',
     }],
   },
   resolve: {
